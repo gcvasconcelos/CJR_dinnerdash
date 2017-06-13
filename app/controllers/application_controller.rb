98b66695f22e3b6b97241d5cf6c61a9e51061125
Yan Galli <yangalli@cjr.org.br>
2017-06-13 09:46:05 -0300

    Category-no_views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  def index
+  	@categories = Category.all
+  end
+  	
+  def show
+  	@category = Category.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+ 
+  def create
+    @category = Category.new(category_params)
+ 
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @category = Category.find(params[:id])
+ 
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+ 
+    redirect_to categorys_path
+  end
+
+  private
+  	def catergory_params
+  		params.require(:category).permit(:validates, :presence)
+  	end
 end
