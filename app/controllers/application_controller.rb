25ceb1808bbbba5bb81ea831d2dbfbb134ca8610
Carlos Silva <carlossilva@cjr.org.br>
2017-06-20 13:14:33 -0300

    broken, just like our hearts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,49 +1,11 @@
 class ApplicationController < ActionController::Base
-  
-  def index
-  	@categories = Category.all
-  end
-  	
-  def show
-  	@category = Category.find(params[:id])
+  protect_from_forgery with: :exception
+
+  before_action :current_order
+
+  def current_order
+    session{:cart} ||= []
+    session{:cart} 
   end
 
-  def new
-  end
-
-  def edit
-    @category = Category.find(params[:id])
-  end
- 
-  def create
-    @category = Category.new(category_params)
- 
-    if @category.save
-      redirect_to @category
-    else
-      render 'new'
-    end
-  end
- 
-  def update
-    @category = Category.find(params[:id])
- 
-    if @category.update(category_params)
-      redirect_to @category
-    else
-      render 'edit'
-    end
-  end
- 
-  def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
- 
-    redirect_to categorys_path
-  end
-
-  private
-  	def catergory_params
-  		params.require(:category).permit(:validates, :presence)
-  	end
 end
