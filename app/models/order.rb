be56e029b3705724d3ee687a5c43bbb7355396de
Yan Galli <yangalli@cjr.org.br>
2017-07-11 21:24:00 -0300

    Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,20 @@
 class Order < ApplicationRecord
 	has_many :items, dependent: :destroy
-	
 	validates :items, presence: true
+	before_create :set_status
+	before_save :update_subtotal
+
+	def subtotal
+		order_items.collect { |i| i.valid? ? (i.quantity * i.unit_price) : 0 }.sum
+	end
+
+	private
+		def set_status
+			self.order_status_id = 1
+		end
+
+		def update_subtotal
+			self[:subtotal] = subtotal
+		end
+	end
 end
